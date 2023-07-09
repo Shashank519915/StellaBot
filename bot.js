88ac7ca8e97c4ed6e31abb1b5594ec8db653cc47
Voxlinou <71925280+makouille@users.noreply.github.com>
2023-07-09 19:59:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
-
-/*
-const { Client, Intents } = require('discord.js');
-const client = new Discord.Client({ 
-  intents: [ 
-    Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MESSAGES,
-    Discord.Intents.FLAGS.DIRECT_MESSAGES
-  ] 
-});
-*/
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { readdirSync } = require('fs')
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -19,21 +9,10 @@ const client = new Client({
 });
 
 const { prefix, token } = require("./config.json");
-const jokeCommand = require("./jokeCommand");
-const greetCommand = require("./greetCommand");
-const weatherCommand = require("./weatherCommand");
-const mentionCommand = require("./mentionCommand");
-const helpCommand = require("./helpCommand");
-const triviaCommand = require("./triviaCommand");
-
-const commands = {
-  joke: jokeCommand,
-  greet: greetCommand,
-  weather: weatherCommand,
-  mention: mentionCommand,
-  help: helpCommand,
-  trivia: triviaCommand,
-};
+const commands = {}
+for(let cmdName of fs.readdirSync('./commands/').replace('.js','')){
+  commands[cmdName] = require('./commands/'+cmdName)
+}
 
 client.once("ready", () => {
   console.log("Bot is ready!");
@@ -41,26 +20,15 @@ client.once("ready", () => {
 
 client.on("messageCreate", (message) => {
   console.log("Message received:", message.content);
-  if (message.author.bot) return; // Ignore messages from other bots
-
-  if (message.content.startsWith(prefix)) {
+  // Ignore messages from other bots
+  if (message.author.bot) return;
+  // IF It's a message, just reply to it
+  if (message.mentions.has(client.user.id)) return commands["mention"].execute(message);
+  // From here, if it's not command, ignore.
+  if (!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    if (command === "joke") {
-      jokeCommand.execute(message);
-    } else if (command === "greet") {
-      greetCommand.execute(message, args);
-    } else if (command === "weather") {
-      weatherCommand.execute(message, args);
-    } else if (command === "help") {
-      helpCommand.execute(message);
-    } else if (command === "trivia") {
-      triviaCommand.execute(message);
-    }
-  } else if (message.mentions.has(client.user)) {
-    commands["mention"].execute(message);
-  }
+    const commandName = args.shift().toLowerCase();
+    if(commands[command]) return commands[command].execute(message);
 });
 
 client.login(token);
