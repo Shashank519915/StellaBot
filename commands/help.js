a0732fa35670fb789c8a0fc7e31f6e789e687abe
Voxlinou <71925280+makouille@users.noreply.github.com>
2023-07-09 20:05:19 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,13 +9,13 @@ module.exports = {
       .setTitle('Stella Bot')
       .setDescription('Here are the available commands:')
       .setThumbnail('https://i.ibb.co/HTNySYF/h.png')
-      .addFields({ name : '!help', value :  'Shows this help message.'})
-      .addFields({ name : '!greet @user', value : 'Greets the user who mentioned the bot.'})
-      .addFields({ name : '!joke', value : 'Generates a random joke.'})
-      .addFields({ name : '!weather (location)', value : 'Sends real-time weather at the location.'})
-      .addFields({ name : '!trivia', value :  'Shows a trivia question that you have to answer in 15 seconds.'})
       .setTimestamp()
-      .setFooter({ text : 'Stella Bot'});
+      .setFooter({ text : 'Stella Bot'})
+      let cmdNames = Object.keys(message.client.commands) // ["greet","help","joke"]
+      .addFields(cmdNames.map(cmdName => ({
+        name: "!"+cmdName,
+        value: message.client.commands[cmdName].description
+      }))
       message.channel.send({embeds:[embed]});
   },
 };
