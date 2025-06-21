ba4ed3904e71997b5c4af5480e5966d9a47b3c62
Shashank Anand <120128150+Shashank519915@users.noreply.github.com>
2025-06-21 16:33:19 +0530

    Update weatherCommand.js

--- Commands/weatherCommand.js
+++ Commands/weatherCommand.js
@@ -1,3 +1,4 @@
+// weatherCommand.js
 const fetch = require('node-fetch').default;
 const apiKey = require('../config.json').weatherapikey;
 
@@ -23,7 +24,40 @@ module.exports = {
         const weatherDescription = data.weather[0].description;
         const temperature = data.main.temp;
 
-        message.channel.send(`The weather in ${location} is ${weatherDescription} with a temperature of ${temperature}°C.`);
+        // Emoji Mapping 
+        const emojiMap = {
+          clear: '☀️',
+          clouds: '☁️',
+          rain: '🌧️',
+          drizzle: '🌦️',
+          thunderstorm: '⛈️',
+          snow: '❄️',
+          mist: '🌫️',
+          smoke: '💨',
+          haze: '🌫️',
+          dust: '🌪️',
+          fog: '🌫️',
+          sand: '🏜️',
+          ash: '🌋',
+          squall: '🌬️',
+          tornado: '🌪️'
+        };
+
+        // Find emoji that matches description keyword
+        const words = weatherDescription.toLowerCase().split(' ');
+        let emoji = '';
+        for (const word of words) {
+          if (emojiMap[word]) {
+            emoji = emojiMap[word];
+            break;
+          }
+        }
+
+        // Default emoji if no match found
+        if (!emoji) emoji = '🌍';
+
+        // Send message with emoji
+        message.channel.send(`The weather in ${location} is ${weatherDescription} ${emoji} with a temperature of ${temperature}°C.`);
       } else {
         message.channel.send('Unable to fetch the weather for the specified location.');
       }
@@ -31,6 +65,5 @@ module.exports = {
       console.error(error);
       message.channel.send('An error occurred while fetching the weather.');
     }
-    },
+  },
 };
-
